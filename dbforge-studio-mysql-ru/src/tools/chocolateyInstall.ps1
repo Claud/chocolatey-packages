7fc475f0c85f4ca4fd1913d11adab6620664829a
Shiryaev Andrey <grabzila@gmail.com>
2014-05-17 14:08:39 +0400

    Add new package - dbforge-studio-mysql-ru

---
+++ dbforge-studio-mysql-ru/src/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+
+$packageName = 'dbforge-studio-mysql-ru' # arbitrary name for the package, used in messages
+$installerType = 'exe' #only one of these: exe, msi, msu
+$url = 'http://www.devart.com/dbforge/mysql/studio/dbforgemysqlru.exe' # download url
+$silentArgs = '/verysilent /norestart' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+# main helpers - these have error handling tucked into them already
+# installer, will assert administrative rights
+# if removing $url64, please remove from here
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
