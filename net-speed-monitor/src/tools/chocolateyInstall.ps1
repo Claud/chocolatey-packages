18af5994a9e3d533835701dab8ed375bac1f1a34
Shiryaev Andrey <grabzila@gmail.com>
2014-05-29 04:29:31 +0400

    fix

--- net-speed-monitor/src/tools/chocolateyInstall.ps1
+++ net-speed-monitor/src/tools/chocolateyInstall.ps1
@@ -8,7 +8,19 @@ $silentArgs = '/quiet' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try 
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
 # main helpers - these have error handling tucked into them already
-# installer, will assert administrative rights
-# if removing $url64, please remove from here
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
-# download and unpack a zip file
+# installer, will assert administrative rights
+
+# Set compatibility mode to Windows 7 if operating system is Windows 8 or higher.
+$WindowsVersion = (Get-WmiObject -class Win32_OperatingSystem).Version
+if ($WindowsVersion -ge "6.2.9200") {
+    if (-not (Test-Path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers")) {
+        New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags" -Name Layers
+    }
+    New-ItemProperty -path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -propertyType String -Name "$env:temp\chocolatey\$packageName\$packageName`Install.$installerType" -value "~ WIN7RTM"
+    Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+    
+    # Delete compatibility mode for ext2fsd installer, because it’s not needed anymore
+    Remove-ItemProperty -path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Name "$env:temp\chocolatey\$packageName\$packageName`Install.$installerType"
+} else {
+    Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+}
