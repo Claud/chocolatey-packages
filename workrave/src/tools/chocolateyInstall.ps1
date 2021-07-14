13bae17e56d18853d00a2e2ad54cafa73bc33af2
Zow" Terry Brugger, PhD <zow@acm.org>
2021-07-14 16:35:18 -0500

    Updating workrave to 1.10.45

--- workrave/src/tools/chocolateyInstall.ps1
+++ workrave/src/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'workrave'
 $installerType = 'exe'
-$url = 'http://downloads.sourceforge.net/project/workrave/workrave/1.10/workrave-win32-v1.10-installer.exe?r=&ts=1457189903&use_mirror=netix'
+$url = 'https://github.com/rcaelers/workrave/releases/download/v1_10_45/workrave-win32-v1.10.45.exe'
 $silentArgs = '/verysilent /norestart'
 $validExitCodes = @(0)
 
